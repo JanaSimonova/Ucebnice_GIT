d518f6f844581932d1b37370b5d9470d9a3cf78c
JanaSimonova <jani.simi@seznam.cz>
2024-01-06 01:40:14 +0100

    Kinda working route
    but has some errors:
    in pages-kap1 can't be
        <div id="aside"></div>
    it makes error MIME type

--- JS/pages.js
+++ JS/pages.js
@@ -5,45 +5,163 @@ const notFound = `
 `;
 
 const index = `
-    <h1>Home</h1>
-    <p>
-        This is a simple demo for a single-page application built in vanilla JS. Using a JS framework to build a small
-        application is a little-bit overkill sometimes, but you may still want the benefits of having a single-page app.
-        This is a good solution to that problem.
-    </p>
-    <p>
-        Check out the source code to see how things work. If you want to try it out for yourself, fork this sandbox and
-        play around with it!
-    </p>
+    <div class="first">
+        <div class="left">
+            <img src="/images/home_image.jpg" class="home_image">
+        </div>
+        <div class="right">
+            <h1>GIT</h1>
+            <p>Nejpoužívanější verzovací systém na světě. </p> 
+                <a href="#">Nauč se ho používat i ty!</a>
+            
+        </div>
+    </div>
 `;
 
+
 const uvod = `
-    <h1>Uvod</h1>
-    <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo ullam, dolore magnam sed perspiciatis sequi
-        quidem voluptatum praesentium eius nesciunt illo quasi consectetur asperiores esse nisi quisquam fugiat
-        exercitationem hic. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum mollitia, animi
-        assumenda ducimus hic dolorem itaque quas eos dolores quo illum qui maiores fuga explicabo officia, praesentium
-        harum expedita ratione?
-    </p>
+
+
+    <div id="aside"></div>
+
+    
+    <div class="learn-text">
+        <h1>Úvod</h1>
+        <p>Git je nejpoužívanější verzovací systém, pomáhá miliónům programátorů při jejich práci. 
+            Snadno se používá, je lehký na pochopení a je zdarma. </p>
+        <p class="text-info">Verzovací systém = systém správy historie souborů, verzí</p>
+        <p>Anglicky: Version Control System, VCS.</p>
+        <p>Tento systém ukládá všechny změny do speciální databáze a můžete se kdykoliv podívat a vrátit se do starší verze projektu/souboru.</p>
+        <p>Také můžete vytvářet tzv. větve, což jsou takové "odchylky" od hlavní cesty práce. 
+            To se dá využít například když zkoušíte nový design a nechcete ztratit předchozí verzi.</p>
+        <p>S Gitem velmi často také pracují týmy více lidí.
+            Možnost větvení je proto skvělé řešení, protože každý člen má jen svou větev a tvoří si vlastní změny.</p>
+        <div class="parent">
+            <img src="images/git_branch.webp" class="learn_img_big">  <!-- dole https://www.g2.com/compare/phpstorm-vs-gitkraken-client -->
+            <div class="image_descr">Příklad větvení v aplikaci GitKraken</div>
+        </div>
+
+        <!-- <h2>Verzovací systém</h2>
+        <p>neco o VCS </p>
+        <a href="https://www.atlassian.com/git/tutorials/what-is-version-control#:~:text=Version%20Control%20Systems%20(VCS,control%20are%20as%20follows.">
+            odkaz</a> -->
+
+        <h2>Výhody VCS</h2>
+            <h3>Kompletní historie změn každého souboru</h3>
+                <p>Zaznamenávají se všechny změny. Vytváření, mazání a úprava souborů. Díky tomu se můžete kdykoliv vrátit a podívat se na provedené změny.</p>
+                <p>Změny se značí krátkými popisky, aby bylo snadné se v nich vyznat.</p>
+                <p class="text-info"><strong>Pozor!</strong> Nezaznamenává se vše při každém uložení souboru. Uživatel si sám určuje kdy a co se bude zálohovat.</p>            
+            <h3>Větvení</h3>
+                <p>Je skvělé při práci v týmech, ale může se hodit i samostatnému vývojáři. Provedené změny jsou pouze na jedné větvi.
+                    Větve se dají zase spojovat dohromady, což umožňuje zkontrolovat kolize mezi nimi a řešit je.
+                    Každý pracovník tak může pracovat na svojí větvi, a ty se pak spojí do jedné.</p>
+            <h3>Oprava chyb</h3>
+                <p>Při výskytu chyby se můžete jednoduše podívat do historie a najít, čím je chyba způsobena.</p>
+
+
+        <h2>Druhy VCS</h2>
+        <!-- https://digilib.k.utb.cz/bitstream/handle/10563/41264/novos%C3%A1d_2017_dp.pdf?sequence=1&isAllowed=y -->
+            <h3>Centralizované (CVCS)</h3>
+                <p>Existuje jeden centrální server, na kterém je kompletní historie změn. Každý uživatel má na svém počítači jen nejaktuálnější verzi.</p>
+                <p class="margin_before">Výhodou je, že všichni členové mohou vidět, na čem pracují ostatní, a je jednodušší na naučení. Administrátoři mohou definovat různá oprávnění.</p>
+                <p class="margin_before">Nevýhodou je, že při výpadku centrálního serveru nelze ukládat ani stahovat žádné verze, a při poškození dat na serveru je vše pryč.</p>
+                <ul>
+                    <li>Subversion</li>
+                    <li>CVS</li>
+                    <li>Perforce</li>
+                </ul>
+                <div class="parent">
+                    <img src="images/image_part_001v2.png" class="learn_img_small"> </div>
+                    <!-- https://www.researchgate.net/figure/Centralized-Version-Control-vs-Distributed-Version-Control_fig2_316553817 -->
+            <h3>Distribuované (decentralizované,DVCS)</h3>
+                <p>Nemá centrální databázi, každý uživatel má celou kopii u sebe. Poté změny "pushne" do sítě, nebo centrálního repozitáře.</p>
+                <p class="text-info">Repozitář = úložiště s historií všech uložených změn, všechny soubory a složky, celý "projekt"</p>
+                <p class="margin_before">Hlavními výhodami je možnost pracovat offline, protože každý uživatel má celý repozitář na svém počítači, což urychluje proces. 
+                    Snadnější přepínání mezi větvemi a verzemi, řešení konfliktů. Větší bezpečnost.</p>
+                <p class="margin_before">Nevýhodou je pak trochu složitější fungování, stále ale určitě pochopitelné.</p>
+                <ul>
+                    <li><strong><u>Git</u></strong></li>
+                    <li>Mercurial</li>
+                    <li>Bazaar</li>
+                </ul>
+                <div class="parent">
+                    <img src="images/image_part_002v2.png" class="learn_img_small"> </div>
+
+        <h2>Další verzovací systémy</h2>
+        <h3>Kdjfkl</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, earum! Incidunt quos eum corrupti, rem architecto ipsum ducimus 
+            consectetur sequi adipisci, beatae obcaecati debitis eaque qui atque eveniet dolores harum.</p>
+
+
+        <br>
+
+
+        <a href="/uvod/kap1" onclick="route()"> kap1 </a>
+        <a href="/uvod/kap2" onclick="route()"> kap2 </a>
+        <a href="/uvod/kap3" onclick="route()"> kap3 </a>
+    </div>
 `;
 
 
 const kap1 = `
-    <h1>Kapitola 1</h1>
-    <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo ullam, dolore magnam sed perspiciatis sequi
-        quidem voluptatum praesentium eius nesciunt illo quasi consectetur asperiores esse nisi quisquam fugiat
-        exercitationem hic. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum mollitia, animi
-        assumenda ducimus hic dolorem itaque quas eos dolores quo illum qui maiores fuga explicabo officia, praesentium
-        harum expedita ratione?
-    </p>
+
+
+
+    <div class="learn-text">
+        <h1>Kapitola 1</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo ullam, dolore magnam sed perspiciatis sequi
+            quidem voluptatum praesentium eius nesciunt illo quasi consectetur asperiores esse nisi quisquam fugiat
+            exercitationem hic. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum mollitia, animi
+            assumenda ducimus hic dolorem itaque quas eos dolores quo illum qui maiores fuga explicabo officia, praesentium
+            harum expedita ratione?
+        </p>
+        <p>blblb</p>
+        <div>ahoj</div>
+        <h1>Kapitola 1</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo ullam, dolore magnam sed perspiciatis sequi
+            quidem voluptatum praesentium eius nesciunt illo quasi consectetur asperiores esse nisi quisquam fugiat
+            exercitationem hic. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum mollitia, animi
+            assumenda ducimus hic dolorem itaque quas eos dolores quo illum qui maiores fuga explicabo officia, praesentium
+            harum expedita ratione?
+        </p>
+        <p>blblb</p>
+        <div>ahoj</div>
+        <h1>Kapitola 1</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo ullam, dolore magnam sed perspiciatis sequi
+            quidem voluptatum praesentium eius nesciunt illo quasi consectetur asperiores esse nisi quisquam fugiat
+            exercitationem hic. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum mollitia, animi
+            assumenda ducimus hic dolorem itaque quas eos dolores quo illum qui maiores fuga explicabo officia, praesentium
+            harum expedita ratione?
+        </p>
+        <p>blblb</p>
+        <div>ahoj</div>
+    </div>
 `;
 
+const kap2 =  [
+    {
+        title: "Kapitola 2",
+        text: `<h1>Kapitola 2</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo ullam, dolore magnam sed perspiciatis sequi
+            quidem voluptatum praesentium eius nesciunt illo quasi consectetur asperiores esse nisi quisquam fugiat
+            exercitationem hic. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum mollitia, animi
+            assumenda ducimus hic dolorem itaque quas eos dolores quo illum qui maiores fuga explicabo officia, praesentium
+            harum expedita ratione?
+        </p>`
+    }
+]
+;
 
 
 
 
 
 
-export {notFound, index, uvod, kap1 };
+
+
+
+export {notFound, index, uvod, kap1, kap2 };
--- JS/router.js
+++ JS/router.js
@@ -1,5 +1,5 @@
-import { notFound, index, uvod, kap1 } from "./pages.js";
-import {kapitoly} from "./kapitoly.js";
+import { notFound, index, uvod, kap1, kap2 } from "./pages.js";
+import { kapitoly } from "./kapitoly.js";
 
 const route = (event) => {
     event = event || window.event;
@@ -11,17 +11,30 @@ const route = (event) => {
 const routes = {
     "notFound": notFound,
     "/": index,
-    "/uvod": uvod,
-    "/kap1": kap1,
+    "/index": index,
+    "/learn" : uvod,
+    "/learn/uvod": uvod,  // nefunguje, je to jen napůl načtený
+    "/uvod/kap1": kap1,
+    "/learn/kap1": kap1,
+    "/uvod/kap2": kap2,
+    "/learn/kap2": kap2,
 };
 
 
 const handleLocation = () => {
     const path = window.location.pathname;
     const pathInt = parseInt(path.substring(1)); //removes slash and parses into int
+    const pathString = path.substring(1)
 
-    // if  the number is more than 0 and in the array of kapitoly
-    if (pathInt>0 && pathInt<=kapitoly.length){
+    // load aside, because it's just loading the content and not reloading full page. So it didn't work anywhere else
+    $(function(){
+        $("#aside").load("aside.html"); 
+    });
+
+    console.log(path)
+
+    // if  the number is in the array of kapitoly
+    if (pathInt > 0 && pathInt <= kapitoly.length) {
 
         // make dynamic page with number of page
         const html = `
@@ -29,16 +42,23 @@ const handleLocation = () => {
             <h1>Vlastní strana</h1>
             <p>Toto je stránka číslo ${pathInt}.</p>
         </div>`;
-        document.getElementById("main-page").innerHTML = html;
+        document.getElementById("main").innerHTML = html;
         
         // + add content of kapitola
         const kapitola = kapitoly[pathInt-1];
-        document.getElementById("main-page").innerHTML += kapitola.text;
+        document.getElementById("main").innerHTML += kapitola.text;
+        
+        // makes a title
+        document.title = kapitoly[pathInt-1].title
     }
-    // else = if its not a number, and the path matches with route. OR it's not found (not  number of kapitola or not defined path)
+    // else = when its not a number, and the path matches with route. OR it's not found (not  number of kapitola or not defined path)
     else {
         const html = routes[path] || routes["notFound"];
-        document.getElementById("main-page").innerHTML = html;
+        document.getElementById("main").innerHTML = html;
+        
+        // makes a title
+        document.title = pathString
+        // document.title = kap2[0].title
     }
 };
 
